3ebda6630c7869c8481547dc1767edaa536a1ed6
Gijs Oortgiese <gijs@oortgiese.com>
2025-06-26 11:12:23 +0200

    Allow bigger json requests

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { WsAdapter } from '@nestjs/platform-ws';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+
+  app.use(json({ limit: '50mb' }));
 
   app.useGlobalPipes(
     new ValidationPipe({
